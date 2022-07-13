7da2d6f3983fcd1f760cc858e3cca7362acc9482
Kayla Lewis <kbubolechka@gmail.com>
2022-07-13 13:45:15 -0400

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+AUTHOR = 'Kayla Lewis'
+SITENAME = 'the decision blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+
+# Social widget
+SOCIAL = (('@Estimatrix', 'https://twitter.com/Estimatrix'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+STATIC_PATHS = ['images']
+STATIC_PATHS = [
+    'extras',
+    ]
+
+EXTRA_PATH_METADATA = {
+    'extras/favicon.ico': {'path': 'favicon.ico'},
+    }
